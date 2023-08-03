9cd2875bee50e08a439102d8ed3302729ea79511
Bailey Bell Loffredo <baileybell.loffredo@gmail.com>
2023-08-03 08:37:59 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
+const num1 = 31;
+const num2 = 2;
 
-// Write your code here
+const multiply = num1 * num2;
+
+console.log(multiply);
+
+const random = Math.floor(Math.random() * Number.MAX_SAFE_INTEGER) + 1;
+
+console.log(random)
+
+const num3 = 20;
+const num4 = 5;
+
+const mod = num3 / num4
+console.log(mod)
+
+const max= Math.max(1,2,3,4,5,20)
+console.log(max)
